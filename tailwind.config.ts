33ee40ece36645ec60db5b1d9d9613fbb363918c
burgues0 <victordpp@outlook.com>
2025-07-05 20:00:40 -0300

    add: add ui components + specify package versions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: ["class", "class"],
+  darkMode: "class",
   theme: {
   	extend: {
   		borderRadius: {
